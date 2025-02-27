b04950f4f8df00c7baa4ce51faf7375f59d4435f
JONGWOONG-SEON <www11288@naver.com>
2025-02-28 00:05:04 +0900

    init

---
+++ src/main/kotlin/com/kotlinpjt/config/JpaConfig.kt
@@ -0,0 +1,12 @@
+package com.kotlinpjt.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig {
+
+
+}
